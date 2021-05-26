0b391924ca0e30dcbd24efd47213f5e9fa1ff24a
sida <sliu1@uvm.edu>
2021-05-25 22:45:46 -0400

    Auto Save

--- 2.make_a_dummy_archive.sh
+++ 2.make_a_dummy_archive.sh
@@ -1,11 +1,21 @@
 #!/bin/sh
+# use docker container to build the archive. (Because machines with small memory might have trouble installing pytorch at all.)
 
-python dummy_pth.py --path model_store/face_lm_3d/models/dummy.pth
+echo "Starting the container..."
+docker run -d --rm --name=torchserve-model-archiver-face_lm_3d \
+    --mount type=bind,source=$PWD,target=/home/model-server/host_folder \
+    face_lm_3d:latest \
+    bash
 
-# pip install torch-model-archiver
-# torch-model-archiver==0.4.0
-torch-model-archiver --model-name face_lm_3d --version 1.0 \
-    --serialized-file model_store/face_lm_3d/models/dummy.pth \
-    --export-path model_store/face_lm_3d/ \
-    --force \
-    --handler handler.py
+docker exec -t torchserve-model-archiver-face_lm_3d \
+    python host_folder/dummy_pth.py --path host_folder/model_store/face_lm_3d/models/dummy.pth
+
+docker exec -t torchserve-model-archiver-face_lm_3d \
+    torch-model-archiver --model-name face_lm_3d --version 1.0 \
+        --serialized-file host_folder/model_store/face_lm_3d/models/dummy.pth \
+        --export-path host_folder/model_store/face_lm_3d/ \
+        --force \
+        --handler host_folder/handler.py
+
+echo "Stopping the container..."
+docker stop torchserve-model-archiver-face_lm_3d
